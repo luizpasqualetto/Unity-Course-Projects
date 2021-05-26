99eab0c8a404e002fdc77bf1a456b5854c1d33d6
LuizPasqualetto <Spazetpastroni@gmail.com>
2021-05-26 13:55:39 -0300

    Roll A Ball Tutorial Initial Commit

---
+++ (Tutorial) Roll a Ball Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Rigidbody rb;
+    public TextMeshProUGUI scoreText;
+    public GameObject winText;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.SetActive(false);
+        count = 0;
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+
+        scoreText.text = "Score : " + count;
+
+        if (count >= 12)
+        {
+            winText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            count++;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
